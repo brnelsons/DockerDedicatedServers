5c907c7b1e38ef442ccaa8cf9624dc34817d6399
bnelson <brnelsons139@gmail.com>
2023-07-24 21:59:22 -0500

    added starbound

---
+++ starbound/update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+username=$1
+
+if [ -z "${username}" ]; then
+	echo "Please provide your steam login name as the first argument."
+	exit
+fi
+
+#!/bin/bash
+steamcmd +force_install_dir /starbound \
+  +login "${username}" \
+  +app_update 211820 validate \
+  +quit
